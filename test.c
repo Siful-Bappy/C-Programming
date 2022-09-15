e260358b7c2d3b77455b439710c4598134a96ba6
Siful-Bappy <bappy.ac.bd@gmail.com>
2022-09-15 10:32:48 +0900

    simple code

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    printf("I am in the class ");
+    printf("This is my project for the test.\n");
+    printf("This is the best thing I can do.\n");
+    return 0;
+}
